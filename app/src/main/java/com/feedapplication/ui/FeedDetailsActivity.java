960f99b582788577e09c4bb5169ed18846e39333
Shubham Chauhan <shubham.chauhan@kelltontech.com>
2019-03-17 03:23:38 +0530

    + initial commit

--- app/src/main/java/com/feedapplication/ui/FeedDetailsActivity.java
+++ app/src/main/java/com/feedapplication/ui/FeedDetailsActivity.java
@@ -1,25 +1,28 @@
 package com.feedapplication.ui;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 
 import com.feedapplication.BaseActivity;
 import com.feedapplication.R;
 import com.feedapplication.constants.Constants;
 import com.feedapplication.database.entity.FeedDetails;
-
-import java.io.Serializable;
+import com.feedapplication.databinding.ActivityFeedDetailsBinding;
 
 public class FeedDetailsActivity extends BaseActivity {
 
     private FeedDetails mFeedDetails;
+    private ActivityFeedDetailsBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_feed_details);
 
         if(getIntent()!=null){
             mFeedDetails = (FeedDetails) getIntent().getSerializableExtra(Constants.FEED_DETAILS_KEY);
         }
+
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_feed_details);
+        mBinding.setFeedDetails(mFeedDetails);
     }
 }
--- app/src/main/java/com/feedapplication/ui/HomeActivity.java
+++ app/src/main/java/com/feedapplication/ui/HomeActivity.java
@@ -4,10 +4,8 @@ import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
-import android.databinding.ViewDataBinding;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 
 import com.feedapplication.BaseActivity;
@@ -30,7 +28,6 @@ public class HomeActivity extends BaseActivity implements FeedDetailsListAdapter
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
 
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_home);
 
